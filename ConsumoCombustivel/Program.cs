e7e1d94798c22c70b62447ef9df381e549066279
Henrique Mendes <henriquemendes13@gmail.com>
2023-08-23 13:04:12 -0300

    Add project files.

---
+++ ConsumoCombustivel/Program.cs
@@ -0,0 +1,10 @@
+﻿//Calcule o consumo médio de um automóvel sendo fornecidos a distância total percorrida (em Km)
+//e o total de combustível gasto (em litros).
+
+double distanciatotal, combustivelgasto, consumomedio;
+
+distanciatotal = double.Parse(Console.ReadLine());
+combustivelgasto = double.Parse(Console.ReadLine());
+consumomedio = distanciatotal / combustivelgasto;
+
+Console.WriteLine(consumomedio.ToString("F3") + "km/l");
